5bc0ae60a02f81cda8059f4232525cc017a76a13
Ian Bettison <ian@bettison.me.uk>
2015-03-18 11:03:35 +0000

    Added comments to show where the last committed changes applied.

--- inc/functions.php
+++ inc/functions.php
@@ -1210,11 +1210,12 @@ function view_timesheet($userId, $pStartDate="", $pEndDate="") {
 						}
 						echo " &nbsp;</div></div>";
 						$loopCount=$arrCount+1;
+
                         $accumulateTime = 0;
+                        // check if the first event is a working event
                         if($lunchDeduction[0]["working_event"] == "Yes") {
                             $accumulateTime = strtotime($timediff);
                         }
-
 						//now loop through multiple events on the same day
 						while(date('Y-m-d',strtotime($date)) == date('Y-m-d',strtotime($events[$loopCount]["event_startdate_time"]))) {
 							$timeFrom = date('H:i:s', strtotime($events[$loopCount]["event_startdate_time"]));
